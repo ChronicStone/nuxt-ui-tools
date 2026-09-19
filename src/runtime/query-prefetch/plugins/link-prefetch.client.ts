10afd5d29f8b9923bd516d9675dc390f58b26625
THAO-Cyprien <cthao.pro@gmail.com>
2026-09-19 15:16:31 +0800

    fix(query-prefetch): import defineNuxtPlugin from #app

--- src/runtime/query-prefetch/plugins/link-prefetch.client.ts
+++ src/runtime/query-prefetch/plugins/link-prefetch.client.ts
@@ -1,8 +1,8 @@
-import { defineNuxtPlugin } from 'nuxt/app'
+import { defineNuxtPlugin } from '#app'
 
 import { prefetchPage } from '../utils/prefetch-page'
 
-export default defineNuxtPlugin({
+export default defineNuxtPlugin<Record<never, never>>({
   name: 'nuxt-ui-tools:query-prefetch',
   setup(nuxtApp) {
     nuxtApp.hook('link:prefetch', (to) => nuxtApp.runWithContext(() => prefetchPage(to)))
